b0e131f18cda70715b51452a0a6a3a365f94c546
Stephen Baker <ste.baker.92@gmail.com>
2016-10-11 10:53:24 +0100

    added comment as to why document download fails

--- app/file.service.ts
+++ app/file.service.ts
@@ -14,6 +14,10 @@ export class FileService {
         var headers = new Headers();
         headers.append('responseType', 'arraybuffer');
 
+        //I think the reason the file gets corrupted is because you are loading res into the blob and you actually want res._body . However _body is a private variable and not accessible.
+        //As of today .blob() and .arrayBuffer() on a http response object have not been implemented in Angular 2.
+        // text() and json() are the only two options but both will garble your body. Have you found a solution? – sschueller Mar 18 at 14:05
+
         this.http.get(this.documentApi + "documents/" + documentId, headers).toPromise().then((data) => {
             var blob = new Blob([data.text()]);
             var url = window.URL.createObjectURL(blob);
