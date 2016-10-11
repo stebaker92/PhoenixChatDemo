64db7d90d9472d07ddcafbf90961d7c3d8c8f405
Stephen Baker <ste.baker.92@gmail.com>
2016-10-11 13:41:15 +0100

    changed urls to use callums apis

--- app/app.settings.ts
+++ app/app.settings.ts
@@ -2,9 +2,12 @@ export class AppSettings {
 
     // URLS
 
-    static documentApi = "http://localhost:60646/";
+    // static documentApi = "http://localhost:60646/";
+    static documentApi = "https://cf247documentapi.ngrok.io/";
 
-    static authApiTokenRoute = "http://localhost:6606/authorization/customer/acquire-token/";
+    // static authApiTokenRoute = "http://localhost:6606/authorization/customer/acquire-token/";
+    static authApiTokenRoute = "https://cf247ccauth.ngrok.io/authorization/customer/acquire-token/";
 
-    static contactCentreApiTokenRoute = "http://localhost:5000/auth/customer/";
+    // static contactCentreApiTokenRoute = "http://localhost:5000/auth/customer/";
+    static contactCentreApiTokenRoute = "https://cf247contactcentre.ngrok.io/auth/customer/";
 }
