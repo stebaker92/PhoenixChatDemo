71cd106e976ed02d176ee8b19604f784783d2c80
Stephen Baker <ste.baker.92@gmail.com>
2016-10-21 09:51:29 +0100

    updated auth url

--- app/app.settings.ts
+++ app/app.settings.ts
@@ -6,7 +6,7 @@ export class AppSettings {
     static documentApi = "https://cf247documentapi.ngrok.io/";
 
     // static authApiTokenRoute = "http://localhost:6606/authorization/customer/acquire-token/";
-    static authApiTokenRoute = "https://cf247ccauth.ngrok.io/authorization/customer/acquire-token/";
+    static authApiTokenRoute = "https://cf247auth.ngrok.io/authorization/customer/acquire-token/";
 
     // static contactCentreApi = "http://localhost:5000/";
     static contactCentreApi = "https://cf247contactcentre.ngrok.io/";
