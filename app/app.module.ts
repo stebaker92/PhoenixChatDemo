8d9029033652586fe0b884d6171204979b0bd35c
Stephen Baker <ste.baker.92@gmail.com>
2017-01-06 16:01:49 +0000

    Fixed path

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule, ElementRef}      from '@angular/core';
+import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent}  from './app.component';
@@ -13,42 +13,22 @@ import {CarListComponent} from "./car-list/car-list.component";
 import {LogoutComponent} from "./logout/logout.component";
 import {LoginComponent} from "./login/login.component";
 import {ChatComponent} from "./chat.component";
-import {NgModel, FormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./message.component";
 import {HttpModule} from "@angular/http";
 import {appRoutes} from "./app.routes";
-// import {appRoutes} from "./app.routes";
-//
-// const routes: Routes = [
-//     {
-//         path: 'login',
-//         component: LoginComponent
-//     },
-//     {
-//         path: 'logout',
-//         component: LogoutComponent
-//     },
-//     {
-//         path: '',
-//         redirectTo: '/login',
-//         pathMatch: 'full'
-//     },
-//     {
-//         path: 'list',
-//         component: CarListComponent
-//     },
-//     {
-//         path: 'detail/:id',
-//         component: CarDetailComponent
-//     }
-// ];
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpModule,
         RouterModule.forRoot(appRoutes)
     ],       // module dependencies
     declarations: [AppComponent, LogoutComponent, LoginComponent, ChatComponent, CarListComponent, MessageComponent, CarDetailComponent],   // components and directives
     bootstrap: [AppComponent],     // root component
-    providers: [CarService, ChatService, FileService, SyncService, UserService]                    // services
+    providers: [CarService, ChatService, FileService, SyncService, UserService,
+        // provideRouter(appRoutes),
+        {provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppModule {
 }
+
